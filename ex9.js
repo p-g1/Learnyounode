2890943d06186be359a5d149960b75ec85ae8da5
Pete <petergarvin1@gmail.com>
2016-10-22 13:36:45 +0000

    works sometimes

--- ex9.js
+++ ex9.js
@@ -1,15 +1,16 @@
-var http = require('http')
-var bl = require('bl')
+var http = require('http');
+var bl = require('bl');
 var urls = [process.argv[2], process.argv[3], process.argv[4]];
 var array = [];
 
 urls.forEach((x,i)=>
-http.get(urls[x], function callback(response){
+
+http.get(x, function callback(response){
   response.pipe(bl(function (err, data) {
     if (err) array[i]=err;
     else array[i]=(data.toString());
-    }))
+    if (array.length == 3) array.forEach(x=>console.log(x));
+    }));
   })
 );
 
-console.log(array);
