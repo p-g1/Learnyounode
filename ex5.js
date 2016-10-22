cd789f0660d914a23798bd29598d387c802f6be7
Pete <petergarvin1@gmail.com>
2016-10-22 11:16:17 +0000

    complete

--- ex5.js
+++ ex5.js
@@ -1,7 +1,8 @@
 var fs = require('fs')
 
-
-    fs.readFile(process.argv[2], 'utf8', function callback(err, data) {  
+    fs.readdir(process.argv[2], function callback(err, list) {  
         if (err) return console.error(err);
-        else console.log(data.split('\n').length-1);
+        else {
+            list.filter(x=>x=x.slice(x.length-(process.argv[3].length+1))=='.'+process.argv[3]).forEach(x=>console.log(x));
+        }
     })
