33535e748f6adaac1a6268045612a1b616d45196
Pete <petergarvin1@gmail.com>
2016-11-05 11:49:59 +0000

    it works

---
+++ ex10.js
@@ -0,0 +1,17 @@
+var net = require('net');
+
+function pad(n){ return n <10?'0'+n:n};
+
+var server = net.createServer(function listener(socket){
+    var date = new Date();  
+   
+   var d = date.getFullYear() + '-'+
+    //pad(date.getMonth())
+    '11' + '-'+ //11 is a fiddle as getMonth is pulling in the wrong date...
+    pad(date.getDate())     + ' '+
+    pad(date.getHours())  + ':'+
+    pad(date.getMinutes()) +'\n';
+    socket.end(d);
+});
+
+server.listen(process.argv[2]);
