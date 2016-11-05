363458c22fa096574b402087029aa833d2fa07d4
Pete <petergarvin1@gmail.com>
2016-11-05 13:04:35 +0000

    works

--- ex11.js
+++ ex11.js
@@ -3,7 +3,7 @@ var http = require('http');
 var filename = process.argv[3]
 
 var server = http.createServer(function(request, response){
-    fs.createReadStream(filename).pipe(response);
+    fs.createReadStream(filename).pipe(response) ;
     
 });
 
